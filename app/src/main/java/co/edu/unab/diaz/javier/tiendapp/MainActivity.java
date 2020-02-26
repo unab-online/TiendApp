699b4efc830883b27d6fd997b3398eec1984707d
Sax <javier030499@gmail.com>
2020-02-26 16:01:00 -0500

    Agrego listview y se creo el objeto producto

--- app/src/main/java/co/edu/unab/diaz/javier/tiendapp/MainActivity.java
+++ app/src/main/java/co/edu/unab/diaz/javier/tiendapp/MainActivity.java
@@ -3,12 +3,34 @@ package co.edu.unab.diaz.javier.tiendapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvProductos;
+    private ArrayList<Producto> productos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productos = new ArrayList<>();
+        productos.add(new Producto("PC Asus", 2000));
+        productos.add(new Producto("Disco Duro", 500));
+        productos.add(new Producto("Memoria USB", 100));
+        productos.add(new Producto("Mouse", 50));
+        productos.add(new Producto("Teclado", 80));
+
+        lvProductos = findViewById(R.id.lvProductos);
+
+        ArrayAdapter miAdaptador = new ArrayAdapter<Producto>(getBaseContext(), android.R.layout.simple_list_item_1, productos);
+
+        lvProductos.setAdapter(miAdaptador);
+
+        setTitle(R.string.txtListado);
     }
 }
---
+++ app/src/main/java/co/edu/unab/diaz/javier/tiendapp/Producto.java
@@ -0,0 +1,34 @@
+package co.edu.unab.diaz.javier.tiendapp;
+
+public class Producto {
+
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String toString(){
+        return nombre + "(" + precio +")";
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+}
