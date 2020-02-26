246200c1c5eca50d48f8bb9c37e8d4a956ff1c44
Aula L 5-4 <l54@academico.unab.ad>
2020-02-26 15:55:17 -0500

    Inicio de la app de Tienda con listas

--- app/src/main/java/co/edu/unab/saavedra/juan/tiendapp/MainActivity.java
+++ app/src/main/java/co/edu/unab/saavedra/juan/tiendapp/MainActivity.java
@@ -3,12 +3,44 @@ package co.edu.unab.saavedra.juan.tiendapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvProductos;
+    private ArrayList<Producto> productos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productos = new ArrayList<>();
+        Producto p1 = new Producto("Pc Asus", 12.5);
+        p1.setDescripcion("Computador marca Asus");
+        productos.add(p1);
+        Producto p2 = new Producto("Airpods pro",250);
+        p2.setDescripcion("Audifonos Apple");
+        productos.add(p2);
+        Producto p3 = new Producto("Iphone 11",700);
+        p3.setDescripcion("Celular Apple");
+        productos.add(p3);
+        Producto p4 = new Producto("Monitor Lg",300);
+        p4.setDescripcion("Monitor de 30' marca LG");
+        productos.add(p4);
+        Producto p5 = new Producto("Cable Iphone",30);
+        //p5.setDescripcion("Cable de corriente para Iphone");
+        productos.add(p5);
+
+        lvProductos = findViewById(R.id.lv_productos);
+
+        ArrayAdapter miAdaptador = new ArrayAdapter<Producto>(getBaseContext(), android.R.layout.simple_list_item_1, productos);
+
+        lvProductos.setAdapter(miAdaptador);
+
+        setTitle(R.string.txt_listado);
     }
 }
--- app/src/main/java/co/edu/unab/saavedra/juan/tiendapp/Producto.java
+++ app/src/main/java/co/edu/unab/saavedra/juan/tiendapp/Producto.java
@@ -1,4 +1,43 @@
 package co.edu.unab.saavedra.juan.tiendapp;
 
 public class Producto {
+
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        if(this.descripcion.equals("")){
+            return  nombre + "\n" +
+                    precio+"$";
+        }else{
+            return  nombre + "\n" +
+                    descripcion + "\n" +
+                    precio+"$";
+        }
+
+    }
 }
