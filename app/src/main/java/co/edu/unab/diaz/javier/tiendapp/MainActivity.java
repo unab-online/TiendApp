6c4352784c67d6ca3fd93feb86a8cb1dd8defc14
Sax <javier030499@gmail.com>
2020-02-26 14:58:32 -0500

    Inicio

---
+++ app/src/main/java/co/edu/unab/diaz/javier/tiendapp/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unab.diaz.javier.tiendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
