7ff338034fa9e64185dd868a0513b2c70bef9b56
Aula L 5-4 <l54@academico.unab.ad>
2020-02-26 16:00:44 -0500

    Nuevo Avance

--- app/src/main/java/co/edu/unab/tiendaapp/MainActivity.java
+++ app/src/main/java/co/edu/unab/tiendaapp/MainActivity.java
@@ -3,12 +3,38 @@ package co.edu.unab.tiendaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvProductos;
+    //private String productos [] =new String[5];
+    private ArrayList <Productos> productos;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productos = new ArrayList<Productos>();
+        productos.add(new Productos( "Llaves Locke & Key", 29000));
+        productos.add(new Productos( "Plutonio", 64500));
+        productos.add(new Productos( "Ak 47", 27999));
+        productos.add(new Productos( "Pierna de Cerdo", 18000));
+        productos.add(new Productos( "Obsidiana", 17000));
+        productos.add(new Productos( "Souls", 0000));
+        productos.add(new Productos( "Cerdo sin una pierna", 195000));
+
+        lvProductos = findViewById(R.id.lvProductos);
+
+        ArrayAdapter miAdaptador = new ArrayAdapter<Productos >(getApplicationContext(), android.R.layout.simple_list_item_1, productos);
+        lvProductos.setAdapter(miAdaptador);
+
+        setTitle("TiendaApp");
     }
 }
---
+++ app/src/main/java/co/edu/unab/tiendaapp/Productos.java
@@ -0,0 +1,35 @@
+package co.edu.unab.tiendaapp;
+
+public class Productos {
+
+    private String Nombre;
+    private String Descripcion;
+    private int Precio;
+
+    public Productos(String nombre, int precio) {
+        this.Nombre = nombre;
+        this.Precio = precio;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public int getPrecio() {
+        return Precio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.Descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return  Nombre + "  " +
+                 Precio;
+    }
+}
