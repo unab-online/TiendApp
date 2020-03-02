a8d9fcedd8f6b69d4fbbc6567f60a29db7618dd5
Aula L 5-4 <l54@academico.unab.ad>
2020-03-02 15:56:11 -0500

    Commit detalle activity

---
+++ app/src/main/java/co/edu/unab/patino/omar/tiendapp/DetalleActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unab.patino.omar.tiendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+    }
+}
