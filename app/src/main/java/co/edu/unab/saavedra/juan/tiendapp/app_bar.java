34dc38b72bcf5d57e3664158ca7f6d5ae8a3a0bf
Aula L 5-4 <l54@academico.unab.ad>
2020-03-06 18:34:07 -0500

    App bar, Float Action Button

---
+++ app/src/main/java/co/edu/unab/saavedra/juan/tiendapp/app_bar.java
@@ -0,0 +1,14 @@
+package co.edu.unab.saavedra.juan.tiendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class app_bar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar);
+    }
+}
