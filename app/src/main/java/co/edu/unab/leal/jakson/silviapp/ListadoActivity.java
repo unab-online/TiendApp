ca55cefae054cb22f5b135aee12a4d50b389677f
jakson leal <jleal338@unab.edu.co>
2020-04-19 16:47:56 -0500

    action_toolbar

--- app/src/main/java/co/edu/unab/leal/jakson/silviapp/ListadoActivity.java
+++ app/src/main/java/co/edu/unab/leal/jakson/silviapp/ListadoActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -68,6 +70,14 @@ public class ListadoActivity extends AppCompatActivity {
             }
         });
 
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(), "tap ", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "tap", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void getFakeData() {
@@ -94,7 +104,7 @@ public class ListadoActivity extends AppCompatActivity {
 
     }
 
-    public void cerrarSesion(View view){
+    public void cerrarSesion(){
 
         SharedPreferences misPreferencias = getSharedPreferences(getString(R.string.misDatos), MODE_PRIVATE);
         SharedPreferences.Editor miEditor = misPreferencias.edit();
@@ -104,6 +114,28 @@ public class ListadoActivity extends AppCompatActivity {
         startActivity(i);
         finish();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            cerrarSesion();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
